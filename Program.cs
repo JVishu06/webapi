6405a5741e4a92d2b2b4f35f362ddcb1258756b6
JVishu06 <jvishu.ai@gmail.com>
2024-12-12 23:23:42 +0530

    'first'

---
+++ Program.cs
@@ -0,0 +1,66 @@
+using webapi.Data;
+using webapi.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure PostgreSQL connection
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("idenitycs")));
+
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().
+    AddRoles<IdentityRole>().
+    AddEntityFrameworkStores<IdentityDbContext>();
+
+builder.Services.AddScoped<IRoleService, RoleService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    option.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddSingleton<MongoWeatherService>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+app.MapIdentityApi<IdentityUser>();
+
+app.UseCors("AllowAll");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
