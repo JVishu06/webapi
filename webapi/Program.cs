d3c602aee0e382affa8d3eb093e7547c284fdcaa
JVishu06 <jvishu.ai@gmail.com>
2024-12-16 00:09:51 +0530

    Update Program.cs

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", corsBuilder =>
     {
-        corsBuilder.WithOrigins("https://jvishu06.github.io/weather-wasm") // Add your GitHub Pages URL
+        corsBuilder.WithOrigin() // Add your GitHub Pages URL
                    .AllowAnyMethod()
                    .AllowAnyHeader();
     });
