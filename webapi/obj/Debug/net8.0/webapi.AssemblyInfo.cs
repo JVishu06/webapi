20697956b143dd186bfa7e94d2adc3bd9927badd
JVishu06 <jvishu.ai@gmail.com>
2024-12-15 22:12:20 +0530

    deployment

--- webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
+++ webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f43268f20b0bd94a64f19f81267046bc87d79f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d47d55c42dc92c163295682e59b748db2e7b3fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
